106d6f5cec9c487390366562a9b0410f1260734d
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-02-03 18:12:40 -0500

    Varibales

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+	// Siempre el método main para ejecutar un programa java
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte edad;
+		
+		edad = 35;
+		
+		System.out.println("Edad: " + edad);
+		
+		edad = 36;
+		
+		System.out.println("Edad: " + edad);
+	}
+
+}
