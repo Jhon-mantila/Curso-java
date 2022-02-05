b90992beda0f2f268a0a3de040f49aed17128c4e
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-02-05 11:27:33 -0500

    Operadores en java

---
+++ src/Constante.java
@@ -0,0 +1,66 @@
+
+public class Constante {
+
+	public static void main (String[] args) {
+		
+		
+		/*OPERADORS
+		 * + SUMA
+		 * - RESTA
+		 * * multiplicación
+		 * (/) division
+		 * 
+		 * LÓGICOS, RELACIONALES, Y BOOLEANOS
+		 * > MENOS QUE
+		 * < MAYOR QUE
+		 * <> MAYOR O MENOR QUE
+		 * != DIFERENTE
+		 * == IGUAL QUE
+		 * && Y LOGICO
+		 * 
+		 * INCREMENTO Y DECREMENTO
+		 * ++ INCREMENTO
+		 * -- DECREMENTO
+		 * +=n INCREMENTO
+		 * -=n DECREMENTO
+		 * 
+		 *  CONCATENACIÓN
+		 *  +
+		 * 
+		 * CONSTANTES
+		 * */
+		
+		int a = 5;
+		int b;
+		
+		b = 7;
+		
+		int c = b + a;
+		
+		System.out.println("Suma: " +  c);
+		
+		c++; // Incremento
+		
+		System.out.println("Incremento el valor de c :"+ c);
+		
+		c+=6;
+		
+		System.out.println("Incremento + 6:" + c);
+		
+		c-=6;
+		
+		System.out.println("Decremento - 6 :" + c);
+		
+		c = b/a;
+		
+		System.out.println("División :" + c);
+		
+		double b1 = 7;
+		double a1 = 5;
+		double z = b1/a1;
+	
+		
+		System.out.println("División con decimales: "+z);
+		
+	}
+}
