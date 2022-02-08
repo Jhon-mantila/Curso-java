dbcb3cbe5735d03d0ead8b42655b585c5170e1cb
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-02-08 18:45:59 -0500

    Constantes

---
+++ src/Constante_y_Operadores.java
@@ -0,0 +1,27 @@
+
+public class Constante_y_Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Constantes
+		
+		final int c = 5;
+		
+		final double pulgadas = 2.54;
+		
+		double cm = 6;
+		
+		double resultado = cm/pulgadas;
+		
+		System.out.println("C:"+ c);
+		
+		System.out.println("En: " + cm +" Centimetros entre: " + pulgadas + " Pulgadas es : " + resultado);
+		
+		
+	}
+
+}
+
+
+
