22e0ba35b97327dc16a5cd3f6630a905156953bc
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-02-09 15:59:44 -0500

    Inicio clase math metodo sqrt

---
+++ src/Caluculos_con_math.java
@@ -0,0 +1,15 @@
+
+public class Caluculos_con_math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double num = 9;
+		
+		double raiz = Math.sqrt(num);
+		
+		System.out.println("la raiz de : " + num + " es: " + raiz);
+		
+	}
+
+}
