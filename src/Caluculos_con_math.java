2fc044f0cdaa183322a50c76668c079aecc4fd3a
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-02-10 18:02:04 -0500

    refundición y mas métodos de la clase math

--- src/Caluculos_con_math.java
+++ src/Caluculos_con_math.java
@@ -10,6 +10,26 @@ public class Caluculos_con_math {
 		
 		System.out.println("La raiz de : " + num + " es: " + raiz);
 		
+		float num1 = 5.85f;
+		
+		int resultado =  Math.round(num1);
+		
+		System.out.println("# redondeado: " + resultado);
+		
+		double num2 = 5.85;
+		
+		//hago una refundición
+		int resultado2 = (int)Math.round(num2);
+		
+		System.out.println("con refundición: "+resultado2);
+		
+		double base = 9;
+		
+		double exponente = 2;
+		
+		int resultado4 = (int) Math.pow(base, exponente);
+		
+		System.out.println("#:" + base + " elevado: " + exponente + " resultado: " + resultado4);
 	}
 
 }
