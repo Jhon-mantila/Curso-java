eb9ad02ec3518e5f97baa2ce0633599b66647595
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-02-12 12:14:29 -0500

    Metódos de cadenas de caracteres equeal y substring

---
+++ src/Manipula_Cadena3.java
@@ -0,0 +1,19 @@
+
+public class Manipula_Cadena3 {
+
+	public static void main(String[] args) {
+		
+		String alumno1, alumno2;
+		
+		alumno1 = "David";
+		
+		alumno2 = "david";
+		
+		//metódo  de comparación cadena de caracteres...
+		System.out.println(alumno1.equals(alumno2));
+		
+		System.out.println(alumno1.equalsIgnoreCase(alumno2));
+		
+		
+	}
+}
---
+++ src/Manipulacion_Cadena2.java
@@ -0,0 +1,17 @@
+public class Manipulacion_Cadena2 {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		
+		String frase = "Hoy es un estupendo día para aprender a programar en java.";
+		
+		//Metódo sub string de la clase string 
+		String frase_resumen = frase.substring(0, 29) + " irnos a la playa y olvidarnos de todo";
+		
+		System.out.println(frase);
+		System.out.println();
+		System.out.println(frase_resumen);
+
+	}
+
+}
