000a39d7b17e734cbd69acd2c19fbecec29a82f4
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-02-26 11:14:30 -0500

    VARIABLES STATIC Y METODOS STATIC

--- src/poo/Constante_prueba.java
+++ src/poo/Constante_prueba.java
@@ -5,13 +5,54 @@ public class Constante_prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		Empleados[] misEmpleados = new Empleados[5];
+		
+		misEmpleados[0] = new Empleados("David");
+		
+		misEmpleados[0].set_seccion("Soporte");
+		
+		misEmpleados[1] = new Empleados("Zhoe");
+		
+		misEmpleados[1].set_seccion("Soporte");
+		
+		misEmpleados[2] = new Empleados("Laura");
+		
+		misEmpleados[3] = new Empleados("Juan");
+		
+		misEmpleados[3].set_seccion("Desarrollo");
+		
+		misEmpleados[4] = new Empleados("Alejandro");
+		
+		misEmpleados[4].set_seccion("Analista");
+		
+		
+		for(Empleados e: misEmpleados) {
+			
+			System.out.println(e.get_datos());
+		}
+		
+		
 		Empleados trabajador = new Empleados("Jhon");
 		
 		Empleados trabajador1 = new Empleados("Jessika");
 		
+		Empleados trabajador2 = new Empleados("Daniela");
+		
+		Empleados trabajador3 = new Empleados("Maria");
+		
 		trabajador.set_seccion("Desarrollo");
+		
+		
 		System.out.println(trabajador.get_datos());
+		
 		System.out.println(trabajador1.get_datos());
+		
+		System.out.println(trabajador2.get_datos());
+		
+		System.out.println(trabajador3.get_datos());
+		
+		System.out.println(Empleados.get_idSiguiente());
 		
 	}
 
@@ -23,12 +64,20 @@ class Empleados{
 	
 	private String seccion;
 	
+	private int id;
+	//static permite crear propiedades unicas que pueden cambiar entre instancias
+	private static int idSiguiente = 1;
+	
+	
 	public Empleados(String nom) {
 		
 		nombre = nom;
 		
 		seccion = "Administración";
 		
+		id = idSiguiente;
+		
+		idSiguiente++;
 	}
 	
 	public void set_seccion(String seccion) {
@@ -38,7 +87,12 @@ class Empleados{
 	
 	public String get_datos() {
 		
-		return "El nombre es: " + nombre + " la Sección es: " + seccion;
+		return "id: " + id + " el nombre es: " + nombre + " la Sección es: " + seccion;
+	}
+	
+	public static String get_idSiguiente() {
+		
+		return "El id que sigue es: " + idSiguiente;
 	}
 	
 	/*public void set_nombre(String nom) {
