e2f0c467be0459e21b14108a6bd742e772519b91
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-03-06 16:23:39 -0500

    Temporizador unsando mi evento  ActionListener (oyente)

---
+++ src/poo/Temporizador_n.java
@@ -0,0 +1,36 @@
+package poo;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class Temporizador_n {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Hora oyente = new Hora();
+		
+		Timer mitemporizador = new Timer(5000, oyente);
+					
+		mitemporizador.start();
+		
+		JOptionPane.showMessageDialog(null, "Pulsa Aceptar para detener");
+		
+		System.exit(0);
+	}
+
+}
+
+class Hora implements ActionListener{
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		Date  la_hora = new Date();
+		
+		System.out.println("te pongo la hora cada 5 sg: " + la_hora);
+		
+	}
+}
