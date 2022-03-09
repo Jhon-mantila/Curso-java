ee3cee8b878aaed4270a011d54ad09c00af193ca
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-03-08 21:55:53 -0500

    propiedades del marco

--- src/graficos/Creando_marco.java
+++ src/graficos/Creando_marco.java
@@ -22,7 +22,17 @@ class Mi_marco extends JFrame {
 	
 	public Mi_marco() {
 		
-		setSize(500,300);
+		/*setSize(500,300);
+		
+		setLocation(300,100);*/
+		
+		setBounds(300,100, 250, 250);
+		
+		//setResizable(false); // no deja redimencionar
+		
+		setExtendedState(JFrame.MAXIMIZED_BOTH); // mazimizar todo
+		
+		setTitle("Mi ventana");
 		
 	}
 	
