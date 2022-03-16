c12c9d99d9a3c291e104400fa5c33f937dfd72e1
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-03-15 21:16:57 -0500

    EVENTO DE FOCO PARTE 1

---
+++ src/eventos/Evento_foco.java
@@ -0,0 +1,92 @@
+package eventos;
+
+import javax.swing.*;
+
+import java.awt.Graphics;
+import java.awt.event.*;
+
+public class Evento_foco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Marco_foco mimarco = new Marco_foco();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
+
+
+class Marco_foco extends JFrame{
+	
+	public Marco_foco() {
+		
+		setTitle("eventos de foco");
+		
+		setBounds(500,100,500,500);
+		
+		//EventoFoco mifoco = new EventoFoco();
+		
+		LaminaFoco lamina = new LaminaFoco();
+		
+		add(lamina);
+		
+		setVisible(true);
+		
+		
+	}
+}
+
+class LaminaFoco extends JPanel{
+	
+	JTextField cuadro1;
+	
+	JTextField cuadro2;
+	
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		//Anulo el Layout
+		setLayout(null);
+		
+		 cuadro1 = new JTextField();
+		 
+		 cuadro2 = new JTextField();
+		
+		 cuadro1.setBounds(120, 10, 150, 20);
+		 
+		 cuadro2.setBounds(120, 50, 150, 20);
+		 
+		 add(cuadro1);
+		 
+		 add(cuadro2);
+		 
+		 EventoFoco mifoco = new EventoFoco();
+		 
+		 cuadro1.addFocusListener(mifoco);
+		 
+		 
+	}
+	
+	private class EventoFoco implements FocusListener{
+
+		@Override
+		public void focusGained(FocusEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void focusLost(FocusEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Has perdido el foco");
+		}
+		
+		
+	}
+}
+
+
