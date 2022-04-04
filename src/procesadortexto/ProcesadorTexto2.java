a301b753b80f1909003b1f8eb4c2a23cbcfa706c
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-04-03 19:19:05 -0500

    TRABAJANDO EVENTOS EN EL PROCESADOR DE TEXTO

--- src/procesadortexto/ProcesadorTexto2.java
+++ src/procesadortexto/ProcesadorTexto2.java
@@ -55,19 +55,19 @@ class LaminaProcesador2 extends JPanel{
 			tamano = new JMenu("Tamaño");
 			
 			/*----------Menu Item de fuentes-----------------*/
-			configura_menu("Arial", "fuente", "", 0, 0);
-			configura_menu("Courier", "fuente", "", 0, 0);
-			configura_menu("Verdana", "fuente", "", 0, 0);
+			configura_menu("Arial", "fuente", "Arial", 9, 12);
+			configura_menu("Courier", "fuente", "Courier", 9, 12);
+			configura_menu("Verdana", "fuente", "Vernada", 9, 12);
 			
 			/*----------Menu Item de estilos-----------------*/
-			configura_menu("Negrita", "estilos", "", 0, 0);
-			configura_menu("Cursiva", "estilos", "", 0, 0);
+			configura_menu("Negrita", "estilos", "", Font.BOLD, 1);
+			configura_menu("Cursiva", "estilos", "", Font.ITALIC, 1);
 			
 			/*----------Menu Item de tamaño-----------------*/
-			configura_menu("12", "tamano", "", 0, 0);
-			configura_menu("14", "tamano", "", 0, 0);
-			configura_menu("20", "tamano", "", 0, 0);
-			configura_menu("24", "tamano", "", 0, 0);
+			configura_menu("14", "tamano", "", 9, 14);
+			configura_menu("16", "tamano", "", 9, 16);
+			configura_menu("20", "tamano", "", 9, 20);
+			configura_menu("24", "tamano", "", 9, 24);
 		
 			mibarra.add(fuente);
 			mibarra.add(estilos);
@@ -103,5 +103,58 @@ class LaminaProcesador2 extends JPanel{
 			
 		}
 		
+		elem_menu.addActionListener(new Gestiona_eventos(rotulo, tipo_letra, estilo, tam));
+		
+	}//Metodo 
+	
+	private class Gestiona_eventos implements ActionListener{
+		
+		String tipo_letra, menu;
+		
+		int estilo_letra, tam_letra;
+		
+		public Gestiona_eventos(String elemento, String letra, int estilo, int tam_letras) {
+			
+			tipo_letra = letra;
+			
+			estilo_letra = estilo;
+			
+			tam_letra = tam_letras;
+			
+			menu = elemento;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+		
+			letras = miarea.getFont();
+			
+			if (menu.equals("Arial")|| menu.equals("Courier") || menu.equals("Verdana")) {
+				
+				estilo_letra = letras.getStyle();
+				
+				tam_letra = letras.getSize();
+				
+			}else if(menu.equals("Cursiva") || menu.equals("Negrita")) {
+				
+				tipo_letra = letras.getFontName();
+				
+				tam_letra = letras.getSize();
+				
+			}else if (menu.equals("14") || menu.equals("16") || menu.equals("20") || menu.equals("24")) {
+				
+				tipo_letra = letras.getFontName();
+				
+				estilo_letra = letras.getStyle();
+			}
+			
+			miarea.setFont(new Font(tipo_letra, estilo_letra, tam_letra));
+			
+			System.out.println("Tipo: " + tipo_letra + " estilo: " + estilo_letra + " tam_letra" + tam_letra);
+		}
+		
 	}
+	
+	
 }
