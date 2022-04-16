645db9f7c7f4af7eb007997b8b81d2810c79e206
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-04-16 13:27:29 -0500

    Dispocision avanzadas cajas BOX PARTE 1

---
+++ src/disposicionencaja/PruebaDispocisiones.java
@@ -0,0 +1,103 @@
+package disposicionencaja;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+
+
+public class PruebaDispocisiones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MarcoCaja mimarco = new MarcoCaja();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		mimarco.setVisible(true);
+	}
+
+}
+
+class MarcoCaja extends JFrame{
+	
+	public MarcoCaja() {
+		
+		setTitle("marco en caja");
+		
+		setBounds(600,350,200,200);
+		
+		
+		JLabel rotulo1 = new JLabel("Nombre");
+		
+		JTextField texto1 = new JTextField(10);
+		
+		texto1.setMaximumSize(texto1.getPreferredSize());
+		
+			Box cajaH1 = Box.createHorizontalBox();
+			
+			cajaH1.add(rotulo1);
+			
+			cajaH1.add(Box.createHorizontalStrut(10));
+			
+			cajaH1.add(texto1);
+			 
+			LaminaConFiguras milamina = new LaminaConFiguras();
+			
+			
+			cajaH1.setBackground(Color.blue);
+		
+		
+		JLabel rotulo2 = new JLabel("Contraseña");
+		
+		JTextField texto2 = new JTextField(10);
+		
+		texto2.setMaximumSize(texto2.getPreferredSize());
+		
+			Box cajaH2 = Box.createHorizontalBox();
+			
+			cajaH2.add(rotulo2);
+			
+			cajaH2.add(Box.createHorizontalStrut(10));
+			
+			cajaH2.add(texto2);
+			
+		JButton boton1 = new JButton("OK");
+		
+		JButton boton2 = new JButton("CANCELADO");	
+
+			Box cajaH3 = Box.createHorizontalBox();
+			
+			cajaH3.add(boton1);
+			
+			cajaH3.add(Box.createGlue());
+			
+			cajaH3.add(boton2);
+			
+		Box cajaVertical = Box.createVerticalBox();
+		
+		cajaVertical.add(cajaH1);
+		cajaVertical.add(cajaH2);
+		cajaVertical.add(cajaH3);
+		
+
+		add(milamina, BorderLayout.CENTER);
+		add(cajaVertical,BorderLayout.NORTH);
+	}
+	
+
+	
+}
+
+class LaminaConFiguras extends JPanel{
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		g.drawRect(10,10, 50,50);
+		
+
+	}
+}
