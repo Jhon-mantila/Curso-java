e2a18dc1278d758179f3926603a805703ba5c032
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-04-25 09:16:34 -0500

    Agregando las cajas y colocando el boton mostrar

--- src/ejercicioventanasemergentes/Marco_dialogos.java
+++ src/ejercicioventanasemergentes/Marco_dialogos.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 class Marco_dialogos extends JFrame{
 	
-	private Lamina_dialogos lamina ;
+	private Lamina_dialogos lamina, lamina_tipo_mensaje, lamina_mensaje, lamina_tipo_opcion, lamina_opcion, lamina_entrada;
 	
 	
 	public Marco_dialogos() {
@@ -16,17 +16,61 @@ class Marco_dialogos extends JFrame{
 		
 		setBounds(500,200,500,500);
 		
+		setLayout(new BorderLayout());
+		
 		JPanel lamina_cuadricula = new JPanel();
 		
 		lamina_cuadricula.setLayout(new GridLayout(2,3));
 		
-		String primeraCaja[]= {"Mensaje", "Confirmar", "Opciones", "Entrada"};
-		
+		String[] primeraCaja= {"Mensaje", "Confirmar", "Opciones", "Entrada"};
+						
 		lamina = new Lamina_dialogos("Tipo", primeraCaja);
 		
+		lamina_tipo_mensaje = new Lamina_dialogos("Tipo Mensaje", new String[] {
+				"ERROR_MESSAGE", 
+				"INFORMATION_MESSAGE",
+				"QUESTION_MESSAGE",
+				"PLAIN_MESSAGE"});
+		
+		lamina_mensaje = new Lamina_dialogos("Mensaje", new String[] {
+				"Cadena",
+				"Icono",
+				"Componente",
+				"Otros",
+				"Object[]"});
+		
+		lamina_tipo_opcion = new Lamina_dialogos("Confirmar", new String[] {
+				"DEFAULT_OPTION",
+				"YES_NO_OPTION",
+				"YES_NO_CANCEL_OPTION",
+				"OK_CANCEL_OPTION"});
+		
+		lamina_opcion = new Lamina_dialogos("Confirmar", new String[] {
+				"String[]",
+				"Icon[]",
+				"Object[]"});
+		
+		lamina_entrada = new Lamina_dialogos("Entrada", new String[] {
+				"Combo de Texto",
+				"Combo"});
+		
 		lamina_cuadricula.add(lamina);
+		lamina_cuadricula.add(lamina_tipo_mensaje);
+		lamina_cuadricula.add(lamina_mensaje);
+		lamina_cuadricula.add(lamina_tipo_opcion);
+		lamina_cuadricula.add(lamina_opcion);
+		lamina_cuadricula.add(lamina_entrada);
 		
-		add(lamina_cuadricula);
+		//AGREGANDO LAMINAS 
 		
+		JPanel lamina_mostrar = new JPanel();
+		
+		JButton boton_mostrar = new JButton("Mostar");
+		
+		lamina_mostrar.add(boton_mostrar);
+		
+		add(lamina_cuadricula, BorderLayout.CENTER);
+		
+		add(lamina_mostrar, BorderLayout.SOUTH);
 	}
 }
