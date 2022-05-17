d0d06c07911bd2b336c072ce1abd8971b284d21d
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-05-16 20:49:23 -0500

    interfaz serializable convertir a byte y recuperar objectos

---
+++ src/serializacion/Serializando.java
@@ -0,0 +1,124 @@
+package serializacion;
+
+import java.io.*;
+import java.util.*;
+
+public class Serializando {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Administrador jefe = new Administrador("Jhon", 2000000, 2022, 05,16);
+		
+		jefe.set_incentivo(500000);
+		
+		//Pasaremos  a bytes
+		Empleado[] personal = new Empleado[3];
+		
+		personal[0] = jefe;
+		
+		personal[1] = new Empleado("Jessika", 1500000, 2022,05,16);
+		
+		personal[2] = new Empleado("Daniela", 2500000, 2022,05,16);
+		
+		try {
+			
+			ObjectOutputStream escribiendo_fichero = new ObjectOutputStream(new FileOutputStream("src/serializacion/prueba.txt"));
+			
+			escribiendo_fichero.writeObject(personal);
+			
+			escribiendo_fichero.close();
+			
+			ObjectInputStream recuperando_fichero = new ObjectInputStream(new FileInputStream("src/serializacion/prueba.txt"));
+			
+			Empleado[] personal_recupeado = (Empleado[])recuperando_fichero.readObject();
+			
+			personal_recupeado.clone();
+			
+			for(Empleado e : personal_recupeado) {
+				
+				System.out.println(e);			
+			}
+			
+		}catch(Exception e) {
+			
+		}
+		
+	}
+
+}
+
+class Empleado implements Serializable{
+	
+	private String nombre;
+	
+	private double sueldo;
+	
+	private Date fecha_contrado;
+	
+	public Empleado(String n, double s, int agno, int mes, int dia) {
+		
+		nombre = n;
+		
+		sueldo = s;
+		
+		GregorianCalendar calendario = new GregorianCalendar(agno, mes-1, dia);
+		
+		fecha_contrado = calendario.getTime();
+	}
+	
+	public String get_nombre() {
+		return nombre;
+	}
+	
+	public double get_sueldo() {
+		return sueldo;
+	}
+	
+	public Date get_fecha() {
+		return fecha_contrado;
+	}
+	
+	public void subirSueldo(double porcentaje) {
+		double aumento = sueldo * porcentaje/100;
+		sueldo+=aumento;
+	}
+	
+	public String toString() {
+		return "Nombre: " + nombre + " , Sueldo: " + sueldo + " Fecha: " + fecha_contrado;
+	}
+	
+}
+
+
+class Administrador extends Empleado{
+	
+	private double incentivo;
+	
+	public Administrador(String n, double s, int agno, int mes, int dia) {
+		
+		super(n,s,agno,mes,dia);
+		
+		incentivo = 0;
+		
+	}
+	
+	public double get_sueldo() {
+		
+		double sueldoBase = super.get_sueldo();
+		
+		return sueldoBase+incentivo;
+		
+	}
+	
+	
+	public void set_incentivo(double b) {
+		
+		incentivo = b;
+	}
+	
+	public String toString() {
+		
+		return super.toString() + " Incentivo: " + incentivo;
+	}
+}
